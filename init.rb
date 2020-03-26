4ee39628327face9ad2445fc42fd2f4016102fb4
IkegzI <kegz@mail.ru>
2020-03-26 16:25:10 +0800

    frame plugin

--- init.rb
+++ init.rb
@@ -5,4 +5,12 @@ Redmine::Plugin.register :sunstrike_ticket_creation_plugin do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+ON_OFF_CONST = [['Включен', 0], ['Выключен', 1]]
+settings default: {}, partial: 'ticket/settings/setting'
+  # require_dependency 'ssr_freelance'
+
+end
+ActionDispatch::Callbacks.to_prepare do
+  # SettingsController.send :include, SsrFreelance::Patches::SettingsControllerPatch
 end
