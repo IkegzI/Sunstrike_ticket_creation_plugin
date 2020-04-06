913720bd5a2634e84881a62cfaa7358079b3044f
IkegzI <kegz@mail.ru>
2020-04-06 18:53:34 +0800

    060420

--- app/controllers/tickets_from_file_controller.rb
+++ app/controllers/tickets_from_file_controller.rb
@@ -3,6 +3,12 @@ class TicketsFromFileController < ApplicationController
   end
   def upload_file
     #:find_attachments, - найти
+    #  csv = CSV.parse(File.read(files.first.diskfile), headers: true) - арсинг с заголовками
+    # CSV.table(File.read(files.first.diskfile), headers: true)
+    # csv.by_row[1].headers
+    # csv.by_row[1].values_at
+    # csv.size.times {|item| csv.by_row[item]}
+    files = find_attachments
     binding.pry
   end
 end
