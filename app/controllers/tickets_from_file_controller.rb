28814540fda074b4c1fbb81d84e667179c70a9d8
IkegzI <kegz11@mail.ru>
2020-05-04 18:08:16 +0800

    04052020

--- app/controllers/tickets_from_file_controller.rb
+++ app/controllers/tickets_from_file_controller.rb
@@ -149,11 +149,31 @@ class TicketsFromFileController < ApplicationController
           author: User.current
       )
       binding.pry
+      begin
       issue.custom_field_values.select { |cf| cf.custom_field_id == Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_project_lead_id'].to_i }.first.value = @tasks[k][:custom][:project_lead] if Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_project_lead_id'] != 'non'
+      rescue
+        puts 'Project-lead is not find!'
+      end
+      begin
       issue.custom_field_values.select { |cf| cf.custom_field_id == Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_art_manager_id'].to_i }.first.value = @tasks[k][:custom][:art_manager] if Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_art_manager_id'] != 'non'
+      rescue
+        puts 'Art-manager is not find!'
+      end
+      begin
       issue.custom_field_values.select { |cf| cf.custom_field_id == Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_freelance_id'].to_i }.first.value = @tasks[k][:custom][:freelancer?] if Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_freelance_id'] != 'non'
+      rescue
+        puts 'Value is not correct!'
+      end
+      begin
       issue.custom_field_values.select { |cf| cf.custom_field_id == Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_fix_estimate_id'].to_i }.first.value = @tasks[k][:custom][:fix_estimate] if Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_fix_estimate_id'] != 'non'
+      rescue
+        puts 'Value is not correct!'
+      end
+      begin
       issue.custom_field_values.select { |cf| cf.custom_field_id == Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_out_rate_id'].to_i }.first.value = @tasks[k][:custom][:external_rate] if Setting.plugin_Sunstrike_ticket_creation_plugin['sunstrike_out_rate_id'] != 'non'
+      rescue
+        puts 'Value is not correct!'
+      end
 
       if issue.validate
         issues_new[k] = issue
