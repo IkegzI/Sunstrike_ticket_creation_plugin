01065c1987c45d82e1146d940b1213488789b45b
IkegzI <kegz@mail.ru>
2020-05-04 23:31:44 +0800

    04052020

--- app/controllers/tickets_from_file_controller.rb
+++ app/controllers/tickets_from_file_controller.rb
@@ -201,7 +201,7 @@ class TicketsFromFileController < ApplicationController
       issues_new.each_key do |key|
         if issues_new[key].parent_id.to_i > 0
           binding.pry
-          issues_new[key].update(parent: issues_new[issues_new[key].parent_id.to_s].id)
+          issues_new[key].parent = issues_new[issues_new[key].parent_id.to_s]
           binding.pry
         else
           issues_new[key].update(parent_id: nil)
