52d3af5ea6b95cd2f62971f02851e23822e12faa
IkegzI <kegz11@mail.ru>
2020-05-04 17:35:30 +0800

    04052020

--- app/controllers/tickets_from_file_controller.rb
+++ app/controllers/tickets_from_file_controller.rb
@@ -173,7 +173,7 @@ class TicketsFromFileController < ApplicationController
       redirect_to issues_path
     else
       # redirect_to tickets_from_file_upload_errors_path, :flash => { :error => (errors_validate.keys.map{|k| ["Задача #{k}. ", errors_validate[k].full_messages].join('')}.join("/\r")) }
-      @errors_str = (errors_validate.keys.map { |k| ["Задача #{k}. ", errors_validate[k].full_messages].join('') }.join("\n"))
+      @errors_str = (errors_validate.keys.map { |k| ["Задача #{k}. ", errors_validate[k].full_messages].join('') }.join("<br \\>"))
       flash[:error] = (errors_validate.keys.map { |k| ["Задача #{k}. ", errors_validate[k].full_messages].join('') }.join("\n"))
       binding.pry
       render action: 'render_tasks_with_errors'
