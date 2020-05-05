0edfe77d6523b8aac6fdf33d3eb96950de96988a
IkegzI <kegz11@mail.ru>
2020-05-05 14:04:41 +0800

    05052020-1

--- app/controllers/tickets_from_file_controller.rb
+++ app/controllers/tickets_from_file_controller.rb
@@ -203,18 +203,12 @@ class TicketsFromFileController < ApplicationController
           parent_id_in_hash = issues_new[key].parent_id.to_s
           id_issue = Issue.find(issues_new[parent_id_in_hash].id)
           root_id_parent = id_issue.id if root_id_parent.nil?
-          # "parent_issue_id"=>"82"
-          # issue = Issue.find(issues_new[key].id)
           Issue.find(issues_new[key].id).update(parent_id: root_id_parent)
-          # Issue.find(issues_new[key].id).parent_issue_id = id_issue.id
           Issue.find(issues_new[key].id).parent_issue_id = root_id_parent
           Issue.find(issues_new[key].id).update(lft: i, rgt: i + 1)
           Issue.find(issues_new[key].id).reload
           Issue.find(issues_new[key].id).update(parent_id: id_issue.id)
           i += 2
-          binding.pry
-
-          # Issue.find(issues_new[key].id).update(parent_id: id_issue.id)
         else
           issues_new[key].update(parent_id: nil)
           root_id_parent = nil
